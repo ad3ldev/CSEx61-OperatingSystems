fd1aeb207afae9d247db5c895d95bc6bff11ec0f
Adel <56457765+aadelafattah@users.noreply.github.com>
2022-03-22 12:51:37 +0200

    fork trial

--- Lab1/simple_shell.c
+++ Lab1/simple_shell.c
@@ -8,65 +8,75 @@
 #include<readline/readline.h>
 #include<readline/history.h>
 
-void parent_main(){
-    register_child_signal(on_child_exit());
-    setup_environment();
-    shell();
-}
+// void parent_main(){
+//     register_child_signal(on_child_exit());
+//     setup_environment();
+//     shell();
+// }
 
-int on_child_exit(){
-    reap_child_zombie();
-    write_to_log_file("Child terminated");
-}
+// int on_child_exit(){
+//     reap_child_zombie();
+//     write_to_log_file("Child terminated");
+// }
 
-void setup_environment(){
-    cd(Current_Working_Directory);
-}
+// void setup_environment(){
+//     cd(Current_Working_Directory);
+// }
 
-void shell(){
-    do
-    {
-        parse_input(read_input());
-        evaluate_expression();
-        switch (input_type)
-        {
-        case shell_builtin:
-            execute_shell_bultin();
-            break;
-        case executable_or_error:
-            execute_command();
-            break;
-        default:
-            break;
-        }
-    } while (command_is_not_exit));
+// void shell(){
+//     do
+//     {
+//         parse_input(read_input());
+//         evaluate_expression();
+//         switch (input_type)
+//         {
+//         case shell_builtin:
+//             execute_shell_bultin();
+//             break;
+//         case executable_or_error:
+//             execute_command();
+//             break;
+//         default:
+//             break;
+//         }
+//     } while (command_is_not_exit));
     
-}
+// }
 
-void execute_shell_bultin(){
-    switch (command_type)
-    {
-    case cd:
-        /* code */
-        break;
-    case echo:
-        /* code */
-        break;
-    case export:
-        /* code */
-        break;
-    default:
-        break;
-    }
-}
+// void execute_shell_bultin(){
+//     switch (command_type)
+//     {
+//     case cd:
+//         /* code */
+//         break;
+//     case echo:
+//         /* code */
+//         break;
+//     case export:
+//         /* code */
+//         break;
+//     default:
+//         break;
+//     }
+// }
 
-void execute_command(){
-    child_id = fork();
-    if child{
-        execvp(command_parsed);
-        print("Error");
-        exit();
-    }else if parent and foreground{
-        waitpid(child);
-    }
+// void execute_command(){
+//     child_id = fork();
+//     if child{
+//         execvp(command_parsed);
+//         print("Error");
+//         exit();
+//     }else if parent and foreground{
+//         waitpid(child);
+//     }
+// }
+
+int main(int argc, char const *argv[])
+{
+    pid_t pid;
+	pid = fork();
+	printf("%d\n", pid);
+	pid = fork();
+	printf("%d\n", pid);
+	return 0;
 }
