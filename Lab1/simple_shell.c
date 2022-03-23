b9bf49e3da96fd85f67be1da57cabbdf72328b50
Adel <56457765+aadelafattah@users.noreply.github.com>
2022-03-23 13:59:49 +0200

    input done

--- Lab1/simple_shell.c
+++ Lab1/simple_shell.c
@@ -1,80 +1,123 @@
+//
+//  main.c
+//  simple_shell
+//
+//  Created by AbdelRahman AbdelFattah on 21/03/2022.
+//
+
 #include <signal.h>
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
-#include<unistd.h>
-#include<sys/types.h>
-#include<sys/wait.h>
-#include<readline/readline.h>
-#include<readline/history.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
-// void parent_main(){
-//     register_child_signal(on_child_exit());
-//     setup_environment();
-//     shell();
-// }
+char input[1024];
+char * list[256];
 
+void execute_shell_bultin(){
+	printf("hey");
+}
+
+void execute_command(){
+//	child_id = fork();
+//	if child{
+//		execvp(command_parsed);
+//		print("Error");
+//		exit();
+//	}else if parent and foreground{
+//		waitpid(child);
+//	}
+}
+
+char *parse_input ( char *input, char *delimit, char *openblock, char *closeblock) {
+	static char *token = NULL;
+	char *lead = NULL;
+	char *block = NULL;
+	int iBlock = 0;
+	int iBlockIndex = 0;
+	if ( input != NULL) {
+		token = input;
+		lead = input;
+	}
+	else {
+		lead = token;
+		if ( *token == '\0') {
+			lead = NULL;
+		}
+	}
+	while ( *token != '\0') {
+		if ( iBlock) {
+			if ( closeblock[iBlockIndex] == *token) {
+				iBlock = 0;
+			}
+			token++;
+			continue;
+		}
+		if ( ( block = strchr ( openblock, *token)) != NULL) {
+			iBlock = 1;
+			iBlockIndex = block - openblock;
+			token++;
+			continue;
+		}
+		if ( strchr ( delimit, *token) != NULL) {
+			*token = '\0';
+			token++;
+			break;
+		}
+		token++;
+	}
+	return lead;
+}
+
+int checkcommand(char * command){
+	if(strcmp(command, "cd") || strcmp(command, "export") || strcmp(command, "echo") ){
+		return 1;
+	}
+	return 0;
+}
+
+void shell(){
+	char *tok;
+	char acOpen[]  = {"\""};
+	char acClose[] = {"\""};
+	do
+	{
+		fgets(cmd, sizeof(cmd), stdin);
+		tok = parse_input( cmd, " ", acOpen, acClose);
+		int i = 0;
+		list[i] = tok;
+		i++;
+		while ( ( tok = parse_input ( NULL, " ", acOpen, acClose)) != NULL) {
+			list[i] = tok;
+			i++;
+		}
+		evaluate_expression();
+		if (checkcommand(list[0])){
+			execute_shell_bultin();
+		}else{
+			execute_command();
+		}
+	} while (strcmp(input, "exit"));
+}
 // int on_child_exit(){
 //     reap_child_zombie();
 //     write_to_log_file("Child terminated");
 // }
-
 // void setup_environment(){
 //     cd(Current_Working_Directory);
 // }
 
-// void shell(){
-//     do
-//     {
-//         parse_input(read_input());
-//         evaluate_expression();
-//         switch (input_type)
-//         {
-//         case shell_builtin:
-//             execute_shell_bultin();
-//             break;
-//         case executable_or_error:
-//             execute_command();
-//             break;
-//         default:
-//             break;
-//         }
-//     } while (command_is_not_exit));
-    
-// }
 
-// void execute_shell_bultin(){
-//     switch (command_type)
-//     {
-//     case cd:
-//         /* code */
-//         break;
-//     case echo:
-//         /* code */
-//         break;
-//     case export:
-//         /* code */
-//         break;
-//     default:
-//         break;
-//     }
-// }
 
-// void execute_command(){
-//     child_id = fork();
-//     if child{
-//         execvp(command_parsed);
-//         print("Error");
-//         exit();
-//     }else if parent and foreground{
-//         waitpid(child);
-//     }
-// }
+void parent_main(){
+//     register_child_signal(on_child_exit());
+//     setup_environment();
+    shell();
+}
 
-int main(int argc, char const *argv[])
-{
-    pid_t pid;
-	pid = fork();
-	printf("%d\n", pid);
+int main(int argc, char const *argv[]){
+	parent_main();
 	return 0;
 }
