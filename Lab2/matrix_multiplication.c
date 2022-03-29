6d0a8c4546eed3bc7549617b50489bd4f570c482
Adel <56457765+aadelafattah@users.noreply.github.com>
2022-03-30 01:24:31 +0200

    done

--- Lab2/matrix_multiplication.c
+++ Lab2/matrix_multiplication.c
@@ -11,15 +11,21 @@
 #include <pthread.h>
 #include <string.h>
 
-typedef struct Element{
+#define MAX_SIZE 20
+
+struct element{
     int row;
     int col;
-}element;
+};
 
 
-int a[20][20];
-int b[20][20];
-int c[20][20];
+int a[MAX_SIZE][MAX_SIZE];
+int b[MAX_SIZE][MAX_SIZE];
+int c_matrix[MAX_SIZE][MAX_SIZE];
+int c_row[MAX_SIZE][MAX_SIZE];
+int c_element[MAX_SIZE][MAX_SIZE];
+int row1 = -1,col1 = -1;
+int row2 = -1,col2 = -1;
 
 int check_if_compatible(int col1, int row2){
 	if(col1 == -1 || row2 == -1){
@@ -60,45 +66,97 @@ void read_file(char * filename, int * row, int * col, int array[20][20]){
             char * temp = malloc(sizeof(int));
             fscanf(fp, "%s", temp);
             array[i][j] = atoi(temp);
+            free(temp);
         }
     }
     fclose(fp);
 }
 
-void per_matrix(int x, int y, int z){
-    for(int i = 0; i<x;i++){
-        for(int j = 0; j<y; j++){
-            c[i][j] = 0;
-            for(int k = 0; k<z; k++){
-                c[i][j]+=a[i][k]*b[k][j];
+void write_files(char * filename){
+    int row = row1;
+    int col = col2;
+
+    // PER MATRIX
+    char * file_matrix = filename;
+    strcat(file_matrix, "_per_matrix.txt");
+    FILE *fp = fopen(file_matrix, "w");
+    fprintf(fp, "Method: A thread per matrix\nrow=%d col=%d\n", row, col);
+    for(int i = 0; i<row1; i++){
+        for(int j = 0; j<col2;j++){
+            fprintf(fp,"%d\t", c_matrix[i][j]);
+        }
+        fprintf(fp,"\n");
+    }
+    fclose(fp);
+
+    // PER ROW
+    char * file_row = filename;
+    strcat(file_row, "_per_row.txt");
+    fp = fopen(file_row, "w");
+    fprintf(fp, "Method: A thread per row\nrow=%d col=%d\n", row, col);
+    for(int i = 0; i<row1; i++){
+        for(int j = 0; j<col2;j++){
+            fprintf(fp,"%d\t", c_row[i][j]);
+        }
+        fprintf(fp,"\n");
+    }
+    fclose(fp);
+
+    // PER ELEMENT
+    char * file_element = filename;
+    strcat(file_element, "_per_element.txt");
+    fp = fopen(file_element, "w");
+    fprintf(fp, "Method: A thread per element\nrow=%d col=%d\n", row, col);
+    for(int i = 0; i<row1; i++){
+        for(int j = 0; j<col2;j++){
+            fprintf(fp,"%d\t", c_element[i][j]);
+        }
+        fprintf(fp,"\n");
+    }
+    fclose(fp);
+}
+
+void per_matrix(){
+    for(int i = 0; i<row1;i++){
+        for(int j = 0; j<col2; j++){
+            c_matrix[i][j] = 0;
+            for(int k = 0; k<col1; k++){
+                c_matrix[i][j]+=a[i][k]*b[k][j];
             }
         }
     }
 }
 
-void per_row(int row, int y, int z){
-    for(int j = 0; j<y; j++){
-        c[row][j] = 0;
-        for(int k = 0; k<z; k++){
-            c[row][j]+=a[row][k]*b[k][j];
+void * per_row(void* row){
+	int current_row = (int) row;
+    for(int j = 0; j<col2; j++){
+        c_row[current_row][j] = 0;
+        for(int k = 0; k<col1; k++){
+            c_row[current_row][j]+=a[current_row][k]*b[k][j];
         }
     }
+	pthread_exit(NULL);
 }
 
-void per_element(element * e, int z){
-    c[e->row][e->col] = 0;
-    for(int k = 0; k<z; k++){
-        c[e->row][e->col]+=a[e->row][k]*b[k][e->col];
+void * per_element(void* element){
+    struct element * current_element;
+    current_element = (struct element *) element;
+    int row = current_element -> row;
+    int col = current_element -> col;
+    free(element);
+    c_element[row][col] = 0;
+    for(int k = 0; k<col1; k++){
+        c_element[row][col]+=a[row][k]*b[k][col];
     }
+    pthread_exit(NULL);
 }
+
 
 int main(int argc, const char * argv[]) {
 	struct timeval stop, start;
 	char * mat1 = malloc(64);
 	char * mat2 = malloc(64);
 	char * matout = malloc(64);
-	int row1 = -1,col1 = -1;
-	int row2 = -1,col2 = -1;
 	if(argc == 4){
 		strncpy(mat1, argv[1], strlen(argv[1]));
 		strncpy(mat2, argv[2], strlen(argv[2]));
@@ -110,53 +168,59 @@ int main(int argc, const char * argv[]) {
 	}
 	strcat(mat1, ".txt");
 	strcat(mat2, ".txt");
-	strcat(matout, ".txt");
     read_file(mat1, &row1, &col1, a);
     read_file(mat2, &row2, &col2, b);
     free(mat1);
     free(mat2);
 	if(check_if_compatible(col1,row2)){
-
         // PER MATRIX
 		gettimeofday(&start, NULL); //start checking time
         printf("\nPer Matrix:\n");
-		per_matrix(row1, col2, col1);
+		per_matrix();
         gettimeofday(&stop, NULL); //end checking time
         printf("Seconds taken %lu\n", stop.tv_sec - start.tv_sec);
-		printf("Microseconds taken: %d\n", stop.tv_usec - start.tv_usec);
+		printf("Microseconds taken: %lu\n", stop.tv_usec - start.tv_usec);
 
         // PER ROW
+        pthread_t threads_per_row[MAX_SIZE];
         gettimeofday(&start, NULL); //start checking time
         printf("\nPer Row:\n");
         for(int i = 0; i< row1;i++){
-            per_row(i, col2, col1);
+            int rc = pthread_create(&threads_per_row[i], NULL, per_row, (void *) i);
+            if(rc){
+                printf("ERROR; from pthread_create()\n");
+            }
         }
         gettimeofday(&stop, NULL); //end checking time
         printf("Seconds taken %lu\n", stop.tv_sec - start.tv_sec);
-		printf("Microseconds taken: %d\n", stop.tv_usec - start.tv_usec);
-
+		printf("Microseconds taken: %lu\n", stop.tv_usec - start.tv_usec);
+        for(int i =0 ;i<MAX_SIZE; i++){
+            pthread_join(threads_per_row[i],NULL);
+        }
+        
         // PER ELEMENT
+        pthread_t threads_per_element[MAX_SIZE * MAX_SIZE];
         gettimeofday(&start, NULL); //start checking time
         printf("\nPer Element:\n");
         for(int i = 0; i< row1 ;i++){
             for(int j = 0; j<col2; j++){
-                element e;
-                e.row = i;
-                e.col = j;
-                per_element(&e, col1);
+                struct element * e = malloc(sizeof(struct element));
+                e->row = i;
+                e->col = j;
+                int rc = pthread_create(&threads_per_element[i*col2 + j], NULL, per_element, (void *) e);
+                if(rc){
+                    printf("ERROR; from pthread_create()\n");
+                }
             }
         }
 		gettimeofday(&stop, NULL); //end checking time
         printf("Seconds taken %lu\n", stop.tv_sec - start.tv_sec);
-		printf("Microseconds taken: %d\n", stop.tv_usec - start.tv_usec);
-		
-        printf("\n");
-        for(int i = 0; i<row1; i++){
-            for(int j = 0; j<col2;j++){
-                printf("%d\t", c[i][j]);
-            }
-            printf("\n");
+		printf("Microseconds taken: %lu\n", stop.tv_usec - start.tv_usec);
+        for(int i =0 ;i<MAX_SIZE * MAX_SIZE; i++){
+            pthread_join(threads_per_element[i],NULL);
         }
+        write_files(matout);
+        free(matout);
 	}else{
 		printf("Dimensions are not compatible\n");
 	}
